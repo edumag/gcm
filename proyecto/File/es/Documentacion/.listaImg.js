61152b1ff7ad3f1f141f328fdbe016ae8f3b247c
edumag <eduardomagrane@gmail.com>
2013-02-14 09:52:14 +0100

    Acabada la actualización del módulo 'imagenes'

--- proyecto/File/es/Documentacion/.listaImg.js
+++
@@ -1,5 +0,0 @@
-var tinyMCEImageList = new Array (
-["mini/proDefecto1.jpg","File/es/Documentacion/.miniaturas/proDefecto1.jpg"],
-["proDefecto1.jpg","File/es/Documentacion/proDefecto1.jpg"],
-["mini/admininstrador_de_temas.png","File/es/Documentacion/.miniaturas/admininstrador_de_temas.png"],
-["admininstrador_de_temas.png","File/es/Documentacion/admininstrador_de_temas.png"]);
