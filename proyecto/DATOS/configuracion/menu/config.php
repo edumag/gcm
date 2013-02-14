9ee5b7767d51bd413f4886a3a55e6b9ba2f497d8
edumag <eduardomagrane@gmail.com>
2013-02-14 09:40:54 +0100

    :mt: 2.1519.2317

--- proyecto/DATOS/configuracion/menu/config.php
+++ proyecto/DATOS/configuracion/menu/config.php
@@ -1,3 +1,6 @@
 <?php
-$config['descartar'][] = 'gdoxygen';
+// Archivo generado automaticamente por GcmConfig Thu Feb 14 8:27:31 CET 2013
+$config['descartar'][]='gdoxygen';
+$config['descartar'][]='thumbnail';
+$config['orden_presentacion'][] = 'inicio@1';
 ?>
