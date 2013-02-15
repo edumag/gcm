fb3f3ed1d37ae20683503f588f96869edd0a2741
edumag <eduardomagrane@gmail.com>
2013-02-15 13:39:27 +0100

    Poner al día

--- proyecto/File/es/Seccion/.listaImg.js
+++
@@ -1,7 +0,0 @@
-var tinyMCEImageList = new Array (
-["mini/ojo-250.png","File/es/Seccion/thumbnail/ojo-250.png"],
-["ojo-250.png","File/es/Seccion/ojo-250.png"],
-["mini/panoramica_com_mamedu.jpg","File/es/Seccion/thumbnail/panoramica_com_mamedu.jpg"],
-["panoramica_com_mamedu.jpg","File/es/Seccion/panoramica_com_mamedu.jpg"],
-["mini/fondo.jpg","File/es/Seccion/thumbnail/fondo.jpg"],
-["fondo.jpg","File/es/Seccion/fondo.jpg"]);
