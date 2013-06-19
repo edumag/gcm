a90df6a3ddcdc32ce177f09006d73147bfc55e8a
edumag <eduardomagrane@gmail.com>
2013-06-19 14:21:36 +0200

    :mt: 2.1624.2502

---
+++ gcm/DATOS/idiomas/GCM_LG_es.php
@@ -0,0 +1,8 @@
+<?php
+// Archivo generado automáticamente por GcmConfig Wed Jun 19 12:22:57 CEST 2013
+$GCM_LG_es['Sin Contenido']='';
+$GCM_LG_es['no encontrado']='';
+$GCM_LG_es['Menú']='';
+$GCM_LG_es['Usuario']='';
+$GCM_LG_es['Contraseña']='';
+?>
