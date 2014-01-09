09300eb052f0da612c52c500bf5f3f5e36c0fe74
edumag <eduardomagrane@gmail.com>
2014-01-09 17:34:37 +0100

    :mt: 2.1763.2741

--- gcm/DATOS/idiomas/GCM_LG_es.php
+++ gcm/DATOS/idiomas/GCM_LG_es.php
@@ -1,5 +1,5 @@
 <?php
-// Archivo generado automáticamente por GcmConfig Thu Jan 9 11:21:04 CET 2014
+// Archivo generado automáticamente por GcmConfig Thu Jan 9 17:20:12 CET 2014
 $GCM_LG_es['Sin Contenido']='';
 $GCM_LG_es['no encontrado']='';
 $GCM_LG_es['Menú']='';
@@ -151,4 +151,5 @@ $GCM_LG_es['Renombrando o moviendo sección']='';
 $GCM_LG_es['Añadir constantes']='';
 $GCM_LG_es['redes_sociales']='';
 $GCM_LG_es['Configuración de redes_sociales']='';
+$GCM_LG_es['mamedu.com']='';
 ?>
