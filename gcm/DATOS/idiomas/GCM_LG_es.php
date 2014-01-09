c3b405d93cb895814286b8443a928c1896fbd0a3
edumag <eduardomagrane@gmail.com>
2014-01-09 11:24:43 +0100

    Salir como evento
    Convertimos salir en evento para que los módulos puedan tener en cuenta la
    salida.
    
    :mt: 2.1761.

--- gcm/DATOS/idiomas/GCM_LG_es.php
+++ gcm/DATOS/idiomas/GCM_LG_es.php
@@ -1,5 +1,5 @@
 <?php
-// Archivo generado automáticamente por GcmConfig Thu Jan 9 10:23:45 CET 2014
+// Archivo generado automáticamente por GcmConfig Thu Jan 9 11:21:04 CET 2014
 $GCM_LG_es['Sin Contenido']='';
 $GCM_LG_es['no encontrado']='';
 $GCM_LG_es['Menú']='';
