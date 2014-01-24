655628c168277f0c72fab56a53acda537b27559f
edumag <eduardomagrane@gmail.com>
2014-01-24 14:13:41 +0100

    Cambios en literales

--- gcm/DATOS/idiomas/GCM_LG_ca.php
+++ gcm/DATOS/idiomas/GCM_LG_ca.php
@@ -1,5 +1,5 @@
 <?php
-// Archivo generado automáticamente por GcmConfig Fri Jan 17 14:16:26 CET 2014
+// Archivo generado automáticamente por GcmConfig Fri Jan 24 12:58:32 CET 2014
 $GCM_LG_ca['Administracion de los diferentes idiomas']='';
 $GCM_LG_ca['Administración']='';
 $GCM_LG_ca['Administración de usuarios']='';
