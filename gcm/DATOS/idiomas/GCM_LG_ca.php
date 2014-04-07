2214447abb157fcfebadf17f13db15fdc2c162c7
edumag <eduardomagrane@gmail.com>
2014-04-07 10:03:44 +0200

    Error con Galeria
    Eliminamos Galeria hasta aplicar la actualización de totbisbal
    
    :mt: 2.1402.

--- gcm/DATOS/idiomas/GCM_LG_ca.php
+++ gcm/DATOS/idiomas/GCM_LG_ca.php
@@ -1,5 +1,5 @@
 <?php
-// Archivo generado automáticamente por GcmConfig Fri Apr 4 19:45:49 CEST 2014
+// Archivo generado automáticamente por GcmConfig Mon Apr 7 9:55:12 CEST 2014
 $GCM_LG_ca['Administracion de los diferentes idiomas']='Administració dels diferents idiomes';
 $GCM_LG_ca['Administración']='';
 $GCM_LG_ca['Administración de usuarios']='Administració d\'usuaris';
