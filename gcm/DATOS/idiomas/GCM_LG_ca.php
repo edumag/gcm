7adb76b22f9d0ac4dac8ac4cfc704fd707f9b79d
edumag <eduardomagrane@gmail.com>
2014-04-08 09:23:46 +0200

    *Modificado cerca3.php paraque se fije en la modali*
    :mt: 2.1851.2969

--- gcm/DATOS/idiomas/GCM_LG_ca.php
+++ gcm/DATOS/idiomas/GCM_LG_ca.php
@@ -1,5 +1,5 @@
 <?php
-// Archivo generado automáticamente por GcmConfig Mon Apr 7 13:39:36 CEST 2014
+// Archivo generado automáticamente por GcmConfig Tue Apr 8 9:23:18 CEST 2014
 $GCM_LG_ca['Administracion de los diferentes idiomas']='Administració dels diferents idiomes';
 $GCM_LG_ca['Administración']='';
 $GCM_LG_ca['Administración de usuarios']='Administració d\'usuaris';
