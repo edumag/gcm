ed0cc7ac1d0b8c81188f9d4b8e02f49e0c24f32d
edumag <eduardomagrane@gmail.com>
2015-01-27 09:39:05 +0100

    inicio

--- gcm/DATOS/idiomas/GCM_LG_es.php
+++ gcm/DATOS/idiomas/GCM_LG_es.php
@@ -1,5 +1,5 @@
 <?php
-// Archivo generado automáticamente por GcmConfig Tue Jan 27 8:47:13 CET 2015
+// Archivo generado automáticamente por GcmConfig Tue Jan 27 9:37:31 CET 2015
 $GCM_LG_es['Sin Contenido']='';
 $GCM_LG_es['no encontrado']='';
 $GCM_LG_es['Menú']='';
