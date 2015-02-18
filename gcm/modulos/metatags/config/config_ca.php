223fb8001e54fd254b5697bc88767388a536855a
edumag <eduardomagrane@gmail.com>
2015-02-18 08:48:03 +0100

    Limpieza

--- gcm/modulos/metatags/config/config_ca.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config_DESC['Titulo']='Titulo del proyecto';
-$config_DESC['Subtitulo']='Subtitulo del proyecto';
-$config_DESC['Descripcion']='Descripción del proyecto, para incluir en los meta tags';
-$config_DESC['keywords']='Palabras clave para los buscadores separadas por un espacio';
-?>
