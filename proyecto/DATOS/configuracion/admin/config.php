bca02ba30f5f86f20ace61b1e50e4c6c2ef24b03
edumag <eduardomagrane@gmail.com>
2015-02-27 10:36:27 +0100

    Sitemap

---
+++ proyecto/DATOS/configuracion/admin/config.php
@@ -0,0 +1,21 @@
+<?php
+// Archivo generado automáticamente por GcmConfig Fri Sep 6 10:26:13 CEST 2013
+$config['Idioma por defecto']='es';
+$config['Módulos activados'][0]='cache_http';
+$config['Módulos activados'][1]='comentarios';
+$config['Módulos activados'][2]='descargables';
+$config['Módulos activados'][3]='indexador';
+$config['Módulos activados'][4]='referencias';
+$config['Módulos activados'][5]='widgets';
+$config['Módulos activados'][6]='aplicar_SEO';
+$config['Módulos activados'][7]='literales';
+$config['Módulos activados'][8]='roles';
+$config['Módulos activados'][8]='mapas';
+$config['Módulos activados'][9]='sitemap';
+$config['Palabra secreta para depurar']='debug';
+$config['Proyecto']='proyecto';
+$config['Sufijo para base de datos']='proyecto_';
+$config['bd_conexion']='sqlite:DATOS/proyecto.db';
+$config['bd_pass']='';
+$config['bd_usuario']='';
+?>
