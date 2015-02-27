1790a5b3374c20180db88ebed55b5340a6d2a882
edumag <eduardomagrane@gmail.com>
2015-02-27 10:29:51 +0100

    Módulo sitemap

---
+++ proyecto/DATOS/eventos/sitemap/eventos_usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file sitemap/eventos_usuario.php
+ * @brief Eventos para sitemap
+ * @defgroup eventos_sitemap Eventos de sitemap
+ * @ingroup modulo_sitemap
+ * @ingroup eventos
+ * @{
+ */
+
+/** Evento de ejemplo */
+$eventos['sitemap.xml']['generar'][1]='';
+
+
+/** @} */
+?>
